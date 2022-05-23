0961dd6da6ca78323939a72cae49d3ec555a6c84
HawkToss <benjacorreaz89@gmail.com>
2022-05-23 00:55:46 -0400

    video 9 gas avanzado

--- funciones.js
+++ funciones.js
@@ -8,3 +8,11 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById("10OMKxtKsZR3qL_pJyeTVFjul95_h2l1OR7d62cNTHMU").getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+
+}
